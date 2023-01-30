ec7319ea55a1f1138fd6855638e2a0b287cc4ef0
T09351 <T09351@W204SISF35031>
2023-01-30 18:40:56 -0400

    Adecuaciones minimas

--- Api.App.WspAdministration/Controllers/WspAdministrationController.cs
+++ Api.App.WspAdministration/Controllers/WspAdministrationController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using System.Runtime.CompilerServices;
 using WSP.ABSTRACTION.ENTITIES.Model;
 using WSP.ABSTRACTION.LOGGER;
 
@@ -39,11 +40,11 @@ namespace Api.App.WspAdministration.Controllers
         [ApiExplorerSettings(GroupName = "v1")]
         [HttpGet("webhooks")]
         [ProducesResponseType(typeof(int), 200)]
-        public ActionResult webhooks(string _parameter)
+        public ActionResult webhooks([FromQuery(Name = "hub.node")] string hub_node, [FromQuery(Name = "hub.challenge")] int hub_challenge, [FromQuery(Name = "hub.verify_token")] string hub_verify)
         {
             try
             {
-                this._logger.Debug(string.Format("ENTRADA: {0}", _parameter));
+                this._logger.Debug(string.Format("ENTRADA: {0}"));
                 var _response = new GenericAnswer() { message = "procesing complete.", code = 200, state = true };
                 this._logger.Debug(string.Format("RESPONSE: {0}", JsonConvert.SerializeObject(_response)));
                 return Ok(_response);
---
+++ WSP.ABSTRACTION.ENTITIES/Model/VerificationModel.cs
@@ -0,0 +1,10 @@
+﻿
+namespace WSP.ABSTRACTION.ENTITIES.Model
+{
+    public class VerificationModel
+    {
+        public string mode { get; set; }
+        public int challenge { get; set; }
+        public string verify_token { get; set; }
+    }
+}
